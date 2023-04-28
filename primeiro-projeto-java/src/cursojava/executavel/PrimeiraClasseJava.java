e90a653b6d174c5f9011539db1372d59c09269a3
Marcelo Oliveira <marcello_85@hotmail.com>
2023-04-28 12:27:12 -0300

    Comentario

--- primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
+++ primeiro-projeto-java/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -3,7 +3,7 @@ package cursojava.executavel;
 public class PrimeiraClasseJava {
 
 	public static void main(String[] args) {
-		
+		/* Printando texto na tela */
 		System.out.println("Meu primeiro programa em Java Eclipse.");
 	}
 }
